7bbc410cd2e73b269bb519aa115efa27d5d2dc10
yboniecabayasomogod <ybonie.cabaya.somogod@gmail.com>
2023-11-16 22:14:27 +0800

    deploy

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/About" element={<About />} />
-      <Route path="/Contact" element={<Contact />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
