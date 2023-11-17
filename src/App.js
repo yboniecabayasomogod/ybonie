02f2dfd1c3e41a0ff411cf02f461fdc224e8b7b1
yboniecabayasomogod <ybonie.cabaya.somogod@gmail.com>
2023-11-17 20:32:43 +0800

    deploy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Contact" element={<Contact />} />
+    </Routes>
   );
 }
 
