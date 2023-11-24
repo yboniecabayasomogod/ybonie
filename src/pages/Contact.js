28abbc69ca79f50467a4dce6e164210ee71864b9
yboniecabayasomogod <ybonie.cabaya.somogod@gmail.com>
2023-11-24 20:37:35 +0800

    Recreate the home by use contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,33 @@
-import React from 'react'
-import Home from './Home'
+import React from "react";
+import ContactCss from "../css/Contact.module.css";
 
 const Contact = () => {
   return (
-    <div><Home></Home></div>
-  )
-}
+    <div className={ContactCss.container}>
+      <div className={ContactCss.headerContainer}>
+        <div className={ContactCss.logo}>logo</div>
+      </div>
+      <div className={ContactCss.navigationContainer}>
+        <div className={ContactCss.navigation}>navigation</div>
+      </div>
+      <div className={ContactCss.sectionArticleAsideContainer}>
+        <div className={ContactCss.sectionArticleContainer}>
+          <div className={ContactCss.sectionContainer}>
+            <div className={ContactCss.section}>section</div>
+          </div>
+          <div className={ContactCss.articleContainer}>
+            <div className={ContactCss.article}>article</div>
+          </div>
+        </div>
+        <div className={ContactCss.asideContainer}>
+          <div className={ContactCss.aside}>aside</div>
+        </div>
+      </div>
+      <div className={ContactCss.footerContainer}>
+        <div className={ContactCss.footer}>footer</div>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
